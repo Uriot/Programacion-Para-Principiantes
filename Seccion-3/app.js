a05316092359d476ead5326097f856996d4c2162
Uriot (Eurizar) <elliotur@gmail.com>
2022-01-06 20:51:36 -0600

    Seccion 3
    Sección 3 del curso

---
+++ Seccion-3/app.js
@@ -0,0 +1,22 @@
+
+
+//console.log(' Hola Mundo ');
+
+let fahrenheit = prompt("ingrese grados Fahrenheint");
+let celsius;
+let k;
+
+celsius = (fahrenheit-32)*(5/9);
+
+console.log(celsius + "c.");
+
+// F a K
+
+k = celsius + 273.15;
+console.log(k + "k.")
+
+
+// centigrados a f
+
+// c a kelvin
+// f a k
